b524d72e88c72e408f56c141ff3820294eb57434
Tom Noogen <tom@noogen.net>
2018-05-09 17:46:26 -0500

    fix lint

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,5 +1,5 @@
 const ApiBuilder = require('claudia-api-builder');
-const AWS = require('AWS');
+const AWS = require('aws-sdk');
 const uuidv5 = require('uuid/v5');
 
 const api = new ApiBuilder();
@@ -7,9 +7,9 @@ const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 const poolData = {
   id: process.env.userPoolId,
   clientId: process.env.userPoolClientId
-}
+};
 
-aws.config.region = process.env.region;
+AWS.config.region = process.env.region;
 
 module.exports = api;
 // references:
@@ -17,17 +17,16 @@ module.exports = api;
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityServiceProvider.html
 
 api.post('/login', request => {
-  const body = request.body;
-  var authData = {
-    username : body.username.trim(),
-    password : body.password.trim(),
+  const authData = {
+    username: request.body.username.trim(),
+    password: request.body.password.trim()
   };
 
   cognitoIdentityServiceProvider.adminInitiateAuth({
     AuthFlow: 'ADMIN_NO_SRP_AUTH',
     AuthParameters: {
-        USERNAME: authData.username,
-        PASSWORD: authData.password
+      USERNAME: authData.username,
+      PASSWORD: authData.password
     },
     ClientId: poolData.clientId,
     UserPoolId: poolData.id
@@ -39,21 +38,20 @@ api.post('/login', request => {
 
 function setOptional(name, val, attrs) {
   if (val) {
-    attrs.push({ Name: name, Value: val.trim() });
+    attrs.push({Name: name, Value: val.trim()});
   }
 }
 
 api.post('/signup', request => {
-  const body = request.body;
-  const customFields =  ['address2', 'city', 'state', 'postal', 'country',
+  const customFields = ['address2', 'city', 'state', 'postal', 'country',
     'is_retired', 'occupation', 'employer', 'email_list_optin_at',
     'pay_type', 'pay_customer_id', 'pay_brand', 'pay_last4',
     'pay_exp_month', 'pay_exp_year'];
-  let { email, password, first_name, last_name } = body;
+  let {email, password} = request.body;
 
   password = password.trim();
   email = email.toLowerCase().trim();
-  username = uuidv5(email, 'email');
+  const username = uuidv5(email, 'email');
 
   // setup required attributes
   const createUserParams = {
@@ -64,36 +62,36 @@ api.post('/signup', request => {
     TemporaryPassword: password,
     UserAttributes: [
       {
-          Name: 'given_name',
-          Value: first_name
+        Name: 'given_name',
+        Value: request.body.first_name
       },
       {
-          Name: 'family_name',
-          Value: last_name
+        Name: 'family_name',
+        Value: request.body.last_name
       },
       {
-          Name: 'email',
-          Value: email
+        Name: 'email',
+        Value: email
       }
     ]
   };
 
   // set internal fields that are not exact match
-  setOptional('address', body.address1, createUserParams.UserAttributes);
-  setOptional('phone_number', body.phone, createUserParams.UserAttributes);
+  setOptional('address', request.body.address1, createUserParams.UserAttributes);
+  setOptional('phone_number', request.body.phone, createUserParams.UserAttributes);
 
   // set internal fields that are exact match
-  setOptional('middle_name', body.middle_name, createUserParams.UserAttributes);
-  setOptional('picture', body.picture, createUserParams.UserAttributes);
-  setOptional('profile', body.profile, createUserParams.UserAttributes);
-  setOptional('timezone', body.timezone, createUserParams.UserAttributes);
+  setOptional('middle_name', request.body.middle_name, createUserParams.UserAttributes);
+  setOptional('picture', request.body.picture, createUserParams.UserAttributes);
+  setOptional('profile', request.body.profile, createUserParams.UserAttributes);
+  setOptional('timezone', request.body.timezone, createUserParams.UserAttributes);
 
   // we set this up earlier as AliasAttributes
   setOptional('preferred_username', email, createUserParams.UserAttributes);
 
   // set custom fields
   customFields.forEach(k => {
-    setOptional(`custom:${k}`, body[k], createUserParams.UserAttributes);
+    setOptional(`custom:${k}`, request.body[k], createUserParams.UserAttributes);
   });
 
   return cognitoIdentityServiceProvider.adminCreateUser(createUserParams).promise().then(data => {
@@ -101,8 +99,8 @@ api.post('/signup', request => {
     const authRequest = {
       AuthFlow: 'ADMIN_NO_SRP_AUTH',
       AuthParameters: {
-          USERNAME: username,
-          PASSWORD: password,
+        USERNAME: username,
+        PASSWORD: password
       },
       ClientId: poolData.clientId,
       UserPoolId: poolData.id
@@ -111,12 +109,12 @@ api.post('/signup', request => {
     return cognitoIdentityServiceProvider.adminInitiateAuth(authRequest).promise();
   }).then(data => {
     console.log('create user response2', data);
-    if (data.ChallengeName == 'NEW_PASSWORD_REQUIRED') {
-      let challengeRequest = {
+    if (data.ChallengeName === 'NEW_PASSWORD_REQUIRED') {
+      const challengeRequest = {
         ChallengeName: 'NEW_PASSWORD_REQUIRED',
         ChallengeResponses: {
-            USERNAME: data.ChallengeParameters.USER_ID_FOR_SRP || username,
-            NEW_PASSWORD: password,
+          USERNAME: data.ChallengeParameters.USER_ID_FOR_SRP || username,
+          NEW_PASSWORD: password
         },
         Session: data.Session,
         ClientId: poolData.clientId,
@@ -131,11 +129,10 @@ api.post('/signup', request => {
 });
 
 api.post('/change-pw', request => {
-  const body = request.body;
   const authData = {
-      token : body.token.trim(),
-      password : body.password.trim(),
-      oldPassword : body.oldpassword.trim()
+    token: request.body.token.trim(),
+    password: request.body.password.trim(),
+    oldPassword: request.body.oldpassword.trim()
   };
   const params = {
     PreviousPassword: authData.oldPassword,
@@ -150,9 +147,8 @@ api.post('/change-pw', request => {
 });
 
 api.post('/forgot-pw', request => {
-  const body = request.body;
   const authData = {
-      username : body.username.trim()
+    username: request.body.username.trim()
   };
   const params = {
     ClientId: poolData.clientId,
@@ -166,11 +162,10 @@ api.post('/forgot-pw', request => {
 });
 
 api.post('/forgot-pw-confirm', request => {
-  const body = request.body;
   const authData = {
-    username : body.username.trim(),
-    confirmationCode: body.confirmationCode.trim(),
-    password: body.password.trim()
+    username: request.body.username.trim(),
+    confirmationCode: request.body.confirmationCode.trim(),
+    password: request.body.password.trim()
   };
   const params = {
     ClientId: poolData.clientId,
